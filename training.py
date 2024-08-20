714c3fbe46eb7ee6fd0e3c2ffdfcd73447f01e75
DoDesCadeaux <dduraku@student.s19.be>
2024-08-20 19:21:23 +0200

    cost_function, gradient, gradient_descent, and feature_scaling

--- training.py
+++ training.py
@@ -3,6 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def model(X, theta):
+    return X.dot(theta)
+
+def cost_function(X, y, theta):
+    m = len(y)
+    return 1 / (2 * m) * np.sum((model(X, theta) - y) ** 2)
+
+def gradient(X, y, theta):
+    m = len(y)
+    return 1/m * X.T.dot(model(X, theta) - y)
+
+def gradient_descent(X, y, theta, learning_rate, iterations=1000):
+    cost_history = []
+    for i in range(0, iterations):
+        if i % 10 == 0:
+            print(theta)
+        theta = theta - learning_rate * gradient(X, y, theta)
+    return theta
+
+def normalize_data(x, y):
+    normalized_feature, normalized_target = np.ones(x.shape), np.ones(y.shape)
+    for i in range(0, len(x)):
+        normalized_feature[i] = (x[i] - x.min()) / (x.max() - x.min())
+        normalized_target[i] = (y[i] - y.min()) / (y.max() - y.min())
+    return normalized_feature, normalized_target
+
 path = 'data.csv'
 data = pd.read_csv(path)
 
@@ -13,16 +39,11 @@ y = np.array(data['price'])
 x = x.reshape(x.shape[0], 1)
 y = y.reshape(y.shape[0], 1)
 
-X = np.hstack((x, np.ones(x.shape)))
+x_scaled, y_scaled = normalize_data(x, y)
+X = np.hstack((x_scaled, np.ones(x.shape)))
 
 theta = np.array([0, 0])
 theta = theta.reshape(theta.shape[0], 1)
 
-def model(X, theta):
-    return X.dot(theta)
-
-print(model(X, theta))
-
-plt.scatter(x, y)
-plt.plot(x, model(X, theta), c='red')
-plt.show()
+theta_final = gradient_descent(X, y, theta, 0.01, 1000)
+print(theta_final)
