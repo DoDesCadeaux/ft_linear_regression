cd495ecf977b8b83c1e8bc29630da9ab8460ee7a
dodescadeaux <dduraku@student.s19.be>
2024-08-21 14:02:57 +0200

    norm training and predict files

--- predict_price.py
+++ predict_price.py
@@ -1,36 +1,46 @@
 import numpy as np
 import pandas as pd
 
-path = 'data.csv'
-data = pd.read_csv(path)
-
-x = data['km']
-y = data['price']
-
 def normalize_data(value, min_val, max_val):
     return (value - min_val) / (max_val - min_val)
+
 
 def denormalize_data(normalized_value, min_val, max_val):
     return normalized_value * (max_val - min_val) + min_val
 
-try:
-    mileage = float(input('Enter a mileage to estimate the price:\n'))
-except ValueError as e:
-    print(e)
-
-try:
-    with open('trained_theta.npy', 'rb') as file:
-        trained_theta = np.load(file)
-except FileNotFoundError as e:
-    print(e)
-    exit(1)
 
 def estimator(mileage, trained_theta, x, y):
     normalized_mileage = normalize_data(mileage, x.min(), x.max())
     normalized_estimation = trained_theta[1] + (normalized_mileage * trained_theta[0])
     return denormalize_data(normalized_estimation, y.min(), y.max())
-    
 
-print(f"thetas: {trained_theta}")
-estimation = estimator(mileage, trained_theta, x, y)
-print(estimation)
+if __name__ == "__main__":
+    path = 'data.csv'
+    data = pd.read_csv(path)
+    
+    x = data['km']
+    y = data['price']
+    
+    try:
+        mileage = float(input('Enter a mileage to estimate the price:\n'))
+    except ValueError as e:
+        print(e)
+        exit(1)
+    
+    try:
+        with open('trained_theta.npy', 'rb') as file:
+            trained_theta = np.load(file)
+    except FileNotFoundError as e:
+        print(e)
+        exit(1)
+        
+    estimation = estimator(mileage, trained_theta, x, y)
+    try:
+        print(f"Estimation price for {mileage} km is {estimation[0]:.2f}$")
+    except IndexError as e:
+        print("Theta is not trained or invalid")
+
+    theta = np.array([0, 0])
+    theta = theta.reshape(theta.shape[0], 1)
+    with open('trained_theta.npy', 'wb') as file:
+        np.save(file, theta)
--- training.py
+++ training.py
@@ -7,13 +7,16 @@ from sklearn.metrics import r2_score
 def model(X, theta):
     return X.dot(theta)
 
+
 def cost_function(X, y, theta):
     m = len(y)
     return 1 / (2 * m) * np.sum((model(X, theta) - y) ** 2)
 
+
 def gradient(X, y, theta):
     m = len(y)
     return 1/m * X.T.dot(model(X, theta) - y) 
+
 
 def gradient_descent(X, y, theta, learning_rate, iterations=1000):
     cost_history = []
@@ -25,6 +28,7 @@ def gradient_descent(X, y, theta, learning_rate, iterations=1000):
         np.save(file, theta)
     return theta, cost_history
 
+
 def normalize_data(x, y):
     normalized_feature, normalized_target = np.ones(x.shape), np.ones(y.shape)
     for i in range(0, len(x)):
@@ -32,37 +36,16 @@ def normalize_data(x, y):
         normalized_target[i] = (y[i] - y.min()) / (y.max() - y.min())
     return normalized_feature, normalized_target
 
+
 def denormalize_data(normalized_value, min_val, max_val):
     return normalized_value * (max_val - min_val) + min_val
+
 
 def my_r2_score(y_hat, y):
     SSR = np.sum((y - y_hat) ** 2)
     SST = np.sum((y - y.mean()) ** 2)
     return 1 - (SSR/SST)
 
-
-path = 'data.csv'
-data = pd.read_csv(path)
-
-x = np.array(data['km'])
-y = np.array(data['price'])
-
-x = x.reshape(x.shape[0], 1)
-y = y.reshape(y.shape[0], 1)
-
-x_scaled, y_scaled = normalize_data(x, y)
-X = np.hstack((x_scaled, np.ones(x.shape)))
-
-theta = np.array([0, 0])
-theta = theta.reshape(theta.shape[0], 1)
-
-
-iterations = 1200
-theta_final, cost_history = gradient_descent(X, y_scaled, theta, 0.1, iterations)
-
-print(theta_final)
-
-predictions = model(X, theta_final)
 
 def plot_or_not():
     try:
@@ -74,7 +57,7 @@ def plot_or_not():
         return -1
     if user_input == 'y':
             plt.scatter(denormalize_data(x_scaled, x.min(), x.max()), denormalize_data(y_scaled, y.min(), y.max()))
-            plt.plot(denormalize_data(x_scaled, x.min(), x.max()), denormalize_data(predictions, y.min(), y.max()), color='red')
+            plt.plot(denormalize_data(x_scaled, x.min(), x.max()), denormalize_data(predictions, y.min(), y.max()), c='r')
             plt.show()
             
             plt.plot(np.arange(0, iterations), cost_history)
@@ -82,6 +65,30 @@ def plot_or_not():
     else:
         return
 
-print(my_r2_score(predictions, y_scaled))
-print(r2_score(y_scaled, predictions))
-plot_or_not()
+
+if __name__ == "__main__":
+    path = 'data.csv'
+    data = pd.read_csv(path)
+    
+    x = np.array(data['km'])
+    y = np.array(data['price'])
+    
+    x = x.reshape(x.shape[0], 1)
+    y = y.reshape(y.shape[0], 1)
+    
+    x_scaled, y_scaled = normalize_data(x, y)
+    X = np.hstack((x_scaled, np.ones(x.shape)))
+    
+    theta = np.array([0, 0])
+    theta = theta.reshape(theta.shape[0], 1)
+    
+    
+    iterations = 1200
+    theta_final, cost_history = gradient_descent(X, y_scaled, theta, 0.1, iterations)
+    
+    print(theta_final)
+    
+    predictions = model(X, theta_final)    
+    print(my_r2_score(predictions, y_scaled))
+    print(r2_score(y_scaled, predictions))
+    plot_or_not()
